976ab08dfe523f0e1981ed3b042599983c206802
Vani Yaji <vaniyaji@Vanis-MBP.domain.name>
2019-08-24 19:46:41 +0530

    Wip

--- controllers/AddWordController.js
+++ controllers/AddWordController.js
@@ -1,11 +1,26 @@
 
+var DictionaryModel = require('../models/DictionaryModel');
+
 module.exports = function(app) {
     
     app.post('/add-word', function(req, res) {
-        if(req.body.data) {
-            console.log('res', req.body.data)
+        var form = new multiparty.Form();
+        form.parse(req, function(err, fields, files) {
+            console.log('request hit', fields)
             res.send('success')
-        }
+        // if(req.body.data) {
+        //     DictionaryModel.create(req.body.data, (error, success) => {
+        //         if(success) {
+        //             console.log("success", success)                    
+        //             res.send('success')
+        //         }
+        //         if(error) {
+        //             res.send(error)
+        //         }
+        //     })
+        // }
+        });
+        
     });
 
 }
--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+var bodyParser = require('body-parser');
 const app = express();
+var mongoose = require('mongoose');
+var config = require('./config')
 const errorHandler = require('./_helpers/error_handlers');
-var bodyParser = require("body-parser");
-
 const AddWordsController = require('./controllers/AddWordController');
+
+// app.use(express.json());
+// app.use(bodyParser.json());
+mongoose.connect(config.getDbConnectionString(), { useNewUrlParser: true })
 AddWordsController(app);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 app.use(cors());
 // global error handler
 app.use(errorHandler);
 
-app.listen(3000)
+app.listen(3001)
